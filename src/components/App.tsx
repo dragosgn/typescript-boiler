be902c64c2d3fc2688838f4f267ebd8e60272440
Dragos Nedelcu <dragos.nedelcu@lokalleads.de>
2018-07-10 17:24:30 +0200

    fixed

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -11,14 +11,16 @@ import {
 } from "./styled";
 import CodeEditor from "./route_editor";
 
-const Root = styled.div``;
+const Root = styled.div`
+  width: 100%;
+`;
 
 const Greeting = () => <h1>Hello there</h1>;
 const Public = () => <h3>Public</h3>;
 const Protected = () => <h3>Protected</h3>;
 
 const fakeAuth = {
-  isAuthenticated: false,
+  isAuthenticated: true,
   authenticate(cb) {
     this.isAuthenticated = true;
     setTimeout(cb, 100); // fake async
@@ -128,18 +130,9 @@ const Home = () => (
 );
 
 export default () => (
-  <div>
+  <Root>
     <AuthButton />
-    <ul>
-      <li>
-        <Link to="/public">Public Page</Link>
-      </li>
-      <li>
-        <Link to="/protected">Protected Page</Link>
-      </li>
-    </ul>
-    <Route path="/public" component={Public} />
     <Route path="/login" component={Login} />
-    <PrivateRoute path="/protected" component={Home} />
-  </div>
+    <PrivateRoute exact path="/" component={Home} />
+  </Root>
 );
